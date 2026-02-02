0d2ca3f77c471616dea343927ebebc0076c131f9
Hilda Huliatus <hildahuliatus22@gmail.com>
2026-02-03 06:22:00 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        // Setiap kali kamu fetch ke /api/external/..., 
-        // Next.js akan "menembakkannya" ke server tujuan secara internal.
-        source: '/api/external/:path*',
-        destination: 'https://rentoka.olifemassage.com/api/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         // Setiap kali kamu fetch ke /api/external/..., 
+//         // Next.js akan "menembakkannya" ke server tujuan secara internal.
+//         source: '/api/external/:path*',
+//         destination: 'https://rentoka.olifemassage.com/api/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
